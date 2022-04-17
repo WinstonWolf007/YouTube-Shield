49aceca80b03bdb459d403088a96947c332d169b
root <ubguertin@gmail.com>
2022-04-17 09:03:59 -0400

    version 3.3.2
    testing the package with selenium

--- Script/commandTest.py
+++ Script/commandTest.py
@@ -1,10 +1,35 @@
 from Bash.command import Command
-import random
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 command = Command()
+options = Options()
+
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
 
 @command.add(only_one_value=str)
-def rand(value):
-    value = value.split(":")
-    print('random number:', random.randint(int(value[0]), int(value[1])))
+def https(value):
+    try:
+        driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
+        driver.get(value)
+
+        print(20*'-'+'\n')
+        print('WebSite name:', driver.title)
+        print('WebSite url:', driver.current_url)
+        print('-')
+        print('App cache:', driver.application_cache)
+        print('-')
+        print('Capabilities:', driver.capabilities)
+        print('-')
+        print('WebBrowser name:', driver.name)
+        print('WebBrowser version:', driver.capabilities['browserVersion'])
+        print('-')
+        print('Driver name:', driver.name)
+        print('Driver version:', driver.capabilities[driver.name]['chromedriverVersion'].split(' ')[0])
+        print(20*'-'+'\n')
+
+        driver.close()
+    except:
+        print('error...')
